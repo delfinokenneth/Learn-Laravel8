0e78b400fb626725f85479046792aa11aee7b6d4
delfinokenneth <kenneth.delfino11@gmail.com>
2022-07-24 23:52:57 +0800

    udemy-master-laravel8-part209

--- app/Traits/Taggable.php
+++ app/Traits/Taggable.php
@@ -5,10 +5,30 @@ namespace App\Models;
 
 trait Taggable {
 
+    protected static function bootTaggable()
+    {
+        static::updating(function ($model)
+        {
+
+        });
+
+        static::created(function ($model)
+        {
+
+        });
+    }
+
     public function tags()
     {
         // return $this->BelongsToMany(Tag::class)->withTimestamps();
         return $this->morphToMany(Tag::class, 'taggable')->withTimestamps();
+    }
+
+    private static function findTagsInContent($content)
+    {
+        preg_match_all('/@([^@]+)@/m', $content, $tags);
+
+        return Tag::whereIn('name', $tags[1] ?? [])->get();
     }
 
 }
