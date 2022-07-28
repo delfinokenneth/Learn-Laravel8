883ce7c84f408a854961cf6e6b4e210de220f744
delfinokenneth <kenneth.delfino11@gmail.com>
2022-07-28 22:27:41 +0800

    udemy-master-laravel8-part212

---
+++ app/Mail/CommentPosted.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Comment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CommentPosted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = 'Comment was posted on your blog post {$this->comment->commentable->title} blog post';
+        return $this->subject($subject)
+        ->view('emails.posts.commented');
+    }
+}
