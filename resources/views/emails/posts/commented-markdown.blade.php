38a27487f0181b96749082397470150dd820e381
delfinokenneth <kenneth.delfino11@gmail.com>
2022-09-05 21:31:09 +0800

    udemy-master-laravel8-part218

--- resources/views/emails/posts/commented-markdown.blade.php
+++ resources/views/emails/posts/commented-markdown.blade.php
@@ -6,7 +6,15 @@ Hi {{  $comment->commentable->user->name }}
 Someone has commented on your blogpost
 
 @component('mail::button', ['url' => route('posts.show', ['post' => $comment->commentable->id])])
-Button Text
+View the blog post
+@endcomponent
+
+@component('mail::button', ['url' => route('users.show', ['user' => $comment->user->id])])
+Visit {{  $comment->user->name }} profile
+@endcomponent
+
+@component('mail::panel')
+{{ $comment->content }}
 @endcomponent
 
 Thanks,<br>
